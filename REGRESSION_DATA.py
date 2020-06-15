9b2e2fa92c0cf1d127eb34bfd683757c2b3fe366
Bassel <mail2bassel@yahoo.com>
2020-06-14 21:58:49 -0400

    <3

---
+++ REGRESSION_DATA.py
@@ -0,0 +1,29 @@
+x = [int(i) for i in input("Enter x values separated by space: ").split()]
+y = [int(i) for i in input("Enter y values separated by space: ").split()]
+HYPB1 = int(input("Enter hypothesis test B1 (if applicable): "))
+
+n = len(x)
+
+B1 = (n * sum([x[i] * y[i] for i in range(len(x))]) - sum(x)*sum(y)) \
+/ ((n*sum([x[i] ** 2 for i in range(len(x))])) - (sum(x) ** 2))
+
+B0 = (sum(y) * sum([x[i] ** 2 for i in range(len(x))]) - (sum(x)*sum([x[i] * y[i] for i in range(len(x))]))) \
+/ ((n*sum([x[i] ** 2 for i in range(len(x))])) - (sum(x) ** 2))
+
+residuals = [(y[i] - (x[i]*B1 + B0)) for i in range(n)]
+SSE = sum([i ** 2 for i in residuals])
+
+Sxy = sum([x[i] * y[i] for i in range(len(x))]) - (sum(x)*sum(y))/n
+Sxx = sum([x[i] ** 2 for i in range(len(x))]) - (sum(x) ** 2)/n
+B1HAT = Sxy / Sxx
+sigmaHat = (SSE / (n-2))**(1/2)
+SE_B1HAT = sigmaHat / (Sxx)**(1/2)
+
+t_nMinus2 = (B1HAT - HYPB1) / SE_B1HAT
+
+print("\nB0 = {}, B1 = {}".format(B0, B1))
+print("Residuals: ", end=" ")
+for i in residuals:
+    print(i, end=" ")
+print("\nSSE: {}".format(SSE))
+print("t_n-2 (test statistic) = {}".format(t_nMinus2))
